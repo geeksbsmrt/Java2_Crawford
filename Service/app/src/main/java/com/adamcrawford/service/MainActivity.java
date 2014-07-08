2399ff540f112b1ba907439d95ce1bcfc751da13
Adam crawford <geeksbsmrt@gmail.com>
2014-07-08 17:46:18 -0400

    Initial Commit of Blank Project

---
+++ Service/app/src/main/java/com/adamcrawford/service/MainActivity.java
@@ -0,0 +1,15 @@
+package com.adamcrawford.service;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
