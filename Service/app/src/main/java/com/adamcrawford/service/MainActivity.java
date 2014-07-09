a8cf68e5d0494dafd7259f4ea9e7693bbb9040ca
Adam crawford <geeksbsmrt@gmail.com>
2014-07-08 20:32:59 -0400

    Import Toon classes and custom view for listview.  Found interesting article on making the listview scroll smoother.  Currently implementing the process but commenting out the old code incase reversion is necessary.

--- Service/app/src/main/java/com/adamcrawford/service/MainActivity.java
+++ Service/app/src/main/java/com/adamcrawford/service/MainActivity.java
@@ -1,10 +1,25 @@
 package com.adamcrawford.service;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.adamcrawford.service.toon.ToonAdapter;
+import com.adamcrawford.service.toon.ToonConstructor;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends Activity {
+
+    private ListView charList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,4 +27,50 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    //method to output values to list
+    private void writeList (JSONObject data) {
+
+        //System.out.println(data.toString());
+
+        //create string array
+        ArrayList<ToonConstructor> toonNames = new ArrayList<ToonConstructor>();
+
+        try {
+
+            //get members array out of returned JSON object
+            JSONArray dataArray = data.getJSONArray("members");
+
+            //loop through array putting member names into string array
+            for (int i=0, j=dataArray.length(); i<j; i++) {
+                JSONObject toon = (JSONObject) dataArray.get(i);
+                //toonNames.add(toon.getJSONObject("character").getString("name"));
+                ToonConstructor tc = new ToonConstructor(toon);
+                toonNames.add(tc);
+            }
+
+            //build listAdapter
+            ToonAdapter guildListAdapter = new ToonAdapter(this, R.id.charList, toonNames);
+            //refresh the data
+            guildListAdapter.notifyDataSetChanged();
+            charList.setAdapter(guildListAdapter);
+            //show view on screen
+            charList.setVisibility(View.VISIBLE);
+            //System.out.println(toonNames);
+
+            //handle errors
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //method to display error to user
+    private void printToast(String message) {
+        //get active context
+        Context c = getApplicationContext();
+        //set length for message to be displayed
+        int duration = Toast.LENGTH_LONG;
+        //create message based on input parameter then display it
+        Toast error = Toast.makeText(c, message, duration);
+        error.show();
+    }
 }
---
+++ Service/app/src/main/java/com/adamcrawford/service/toon/ToonAdapter.java
@@ -0,0 +1,75 @@
+package com.adamcrawford.service.toon;
+
+/**
+ * Author:  Adam Crawford
+ * Project: Service
+ * Package: com.adamcrawford.service.toon
+ * File:    ToonAdapter
+ * Purpose: TODO Minimum 2 sentence description
+ */
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.adamcrawford.service.R;
+
+import java.util.ArrayList;
+
+
+public class ToonAdapter extends ArrayAdapter<ToonConstructor>
+{
+    private Context context;
+    private ArrayList<ToonConstructor> objects;
+
+    public ToonAdapter(Context context, int resource, ArrayList<ToonConstructor> objects)
+    {
+        super(context, resource, objects);
+        this.context = context;
+        this.objects = objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        ViewHolder holder;
+        ToonConstructor toon = objects.get(position);
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.item_toon, null);
+            holder = new ViewHolder();
+            holder.toonClassView = (TextView) convertView.findViewById(R.id.toonClass);
+            holder.toonLevelView = (TextView) convertView.findViewById(R.id.toonLevel);
+            holder.toonNameView = (TextView) convertView.findViewById(R.id.toonName);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        //View listItemView = inflater.inflate(R.layout.item_toon, null);
+
+
+        //TextView toonNameView = (TextView) listItemView.findViewById(R.id.toonName);
+        holder.toonNameView.setText(toon.toonName);
+
+        //TextView toonLevelView = (TextView) listItemView.findViewById(R.id.toonLevel);
+        holder.toonLevelView.setText(toon.toonLevel);
+
+        //TextView toonClassView = (TextView) listItemView.findViewById(R.id.toonClass);
+        holder.toonClassView.setText(toon.tnClass);
+
+       // return listItemView;
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        TextView toonNameView;
+        TextView toonLevelView;
+        TextView toonClassView;
+    }
+}
