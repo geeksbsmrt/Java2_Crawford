485e73a8c2a2e9150182878f5a05eb41e7ed46a3
Adam Crawford <geeksbsmrt@gmail.com>
2014-07-09 21:32:31 -0400

    Add service class, modify app layout and introduce descriptions and informative text.  Add multiple data handling methods.

--- Service/app/src/main/java/com/adamcrawford/service/MainActivity.java
+++ Service/app/src/main/java/com/adamcrawford/service/MainActivity.java
@@ -2,8 +2,13 @@ package com.adamcrawford.service;
 
 import android.app.Activity;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -20,11 +25,89 @@ import java.util.ArrayList;
 public class MainActivity extends Activity {
 
     private ListView charList;
+    private static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Boolean isConnected = getStatus(this);
+
+        //get and create onclick for button
+        Button submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (isConnected) {
+                    //dismiss keyboard
+                    InputMethodManager manager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    manager.hideSoftInputFromWindow(v.getWindowToken(), 0);
+
+                    //pull selected name out of spinner
+                    String realmName = "Llane";
+                    //			    System.out.println(realmName);
+
+                    //pull entered user text
+                    //String guild = guildName.getText().toString().replace(" ", "%20");
+                    String guild = "Remnants of Sanity";
+
+                    //ensure entry in guildEdit
+                    if (! guild.equals("")) {
+                        //call method to populate members list
+                        updateList(realmName, guild);
+                    } else {
+                        //warn if edit is blank
+                        printToast(getString(R.string.noEntry));
+                    }
+                } else {
+                    //Throw not connected message
+                    Log.i(TAG, "You are not connected");
+                    printToast(getString(R.string.notConnected));
+                }
+
+            }
+        });
+
+    }
+
+    //method to check for connectivity
+    private Boolean getStatus(Context c){
+        //Log.i(TAG, "In getStatus");
+        //build connectivity manager and network info
+        ConnectivityManager conMan = (ConnectivityManager) c.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = conMan.getActiveNetworkInfo();
+
+        //true/false based on connectivity
+        return netInfo != null && netInfo.isConnected();
+    }
+
+    //method to get information to put into listview
+    private void updateList (String realm, String guild) {
+
+        //call class to connect to network and pull info based on realm selection and input guild
+        //JSONObject toons = new CharSync().execute(realm, guild).get();
+        //TODO Use this to get information from local file
+        JSONObject toons = null;
+        try {
+            toons = new JSONObject().put("char1","toonName");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        //check for data inside JSON object
+        if (toons != null) {
+            //data exists - put it out to screen
+            writeList(toons);
+        } else {
+            //data does not exist
+
+            //clear screen if successful query previously run
+            charList.setVisibility(View.GONE);
+            //throw error to screen
+            printToast(getString(R.string.notFound));
+        }
     }
 
     //method to output values to list
---
+++ Service/app/src/main/java/com/adamcrawford/service/data/SyncService.java
@@ -0,0 +1,23 @@
+package com.adamcrawford.service.data;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Author:  Adam Crawford
+ * Project: Service
+ * Package: com.adamcrawford.service.data
+ * File:    SyncService
+ * Purpose: TODO Minimum 2 sentence description
+ */
+public class SyncService extends IntentService {
+
+    public SyncService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
